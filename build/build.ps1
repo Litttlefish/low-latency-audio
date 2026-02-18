b4cd77ddd4f1f1a3b9e323d199b917784a34ead5
Litttle_fish <38809254+Litttlefish@users.noreply.github.com>
2026-02-18 21:55:09 +0800

    Update build.ps1

--- build/build.ps1
+++ build/build.ps1
@@ -35,7 +35,7 @@ Write-Host ""
 
 if ($env:GITHUB_ACTIONS -eq 'true') {
     Write-Host "CI Environment detected. Ensuring WDK is installed..." -ForegroundColor Cyan
-    $wdkExtensionPath = "$env:WindowsSdkDir\Vsix"
+    $wdkExtensionPath = Join-Path $env:WindowsSdkDir "Vsix"
     if (-not (Test-Path $wdkExtensionPath)) {
         winget install --source winget --exact --id Microsoft.WindowsSDK.10.0.26100
         winget install --source winget --exact --id Microsoft.WindowsWDK.10.0.26100
@@ -197,6 +197,7 @@ Write-Host "Staging folder : $stagingFolder"
 Write-Host "Release folder : $releaseFolder"
 
 Write-Host ""
+
 
 
 
